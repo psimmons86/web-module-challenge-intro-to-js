968a2f59e7ce764d0221d1ace600be062d1a650c
Patrick Simmons <hadroncollides@gmail.com>
2023-01-11 21:41:19 -0800

    tasks 1-3

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ Do the following:
    HINT: no function required
 */
 const votingAge = 36
-if (votingAge > 18){
+if (votingAge >= 18){
   console.log('True')
 } else {
   console.log ('false')
@@ -90,8 +90,9 @@ Do the following:
  3. Return the newly calculated age
 */
 
-function dogYears(/*add your code here*/){
-  /*add your code here*/
+function dogYears(humanYears){
+  return humanYears*7
+  
 }
 
 
@@ -142,7 +143,33 @@ NOTE 2: This is a great time to check the tests to see what it expects, versus w
         So, on this one test, the weight would be 4 pounds, and the age would be 1 years old. It's expecting your function to return a decimal number of 0.2
 */  
 
-function hungryDog(/*add your code here*/){
+function hungryDog(weight,age){
+  if (age >= 1 && weight <= 5) {
+return weight * 0.05;
+  }
+  else if (age>= 1 && weight > 5 && weight <= 10){
+    return weight * 0.04;
+  }
+  else if (age >= 1 && weight >10 && weight <= 15)
+  {
+    return weight * 0.03;
+  }
+  else if (age >=1 && weight > 15) {
+    return weight * 0.02;
+  }
+  else if (age <1 && age >= 0.583){
+    return weight * 0.04;
+  }
+  else if (age < 0.583 && age >= 0.333) {
+    return weight * 0.05;
+  }
+  else if (age < 0.333) {
+    return weight * 0.10;
+  }
+  else {
+    return 'please try again';
+  }
+
   /*add your code here*/
 }
 
