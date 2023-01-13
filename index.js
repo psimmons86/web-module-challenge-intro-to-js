a73570c249dc8202d99083d227299436015a7c7c
Patrick Simmons <hadroncollides@gmail.com>
2023-01-12 23:08:55 -0800

    tasks1-6

--- index.js
+++ index.js
@@ -196,8 +196,33 @@ Use the game function below to do the following:
 RULES OF THE GAME: Scissors beats Paper | Paper beats Rock | Rock beats Scissors | Or there's a tie
 */
 
-function game(user, computer){
+let computer = Math.random();
+if (computer <= 0.34){
+  computer = 'rock';
+}
+else if (computer <= 0.67) {
+  computer = 'paper';
+}
+else if (computer > 0.67) {
+  computer = `scissors`;
+}
+function game(user, computer) {
   /*add your code here*/
+  
+  if (user === computer) {
+    return `it's a tie`;
+  }else if (user === `rock` && computer === `scissors`) 
+  {
+    return 'you win!'
+  }else if (user === 'paper' && computer === 'rock')
+  {
+    return 'you win!'
+  }else if (user === 'scissors' && computer === 'paper')
+  {
+    return 'you win!'
+  }else {
+    return 'you lose!'
+  }
 }
 
 
@@ -213,7 +238,8 @@ Using the miles function below do the following:
 3. Return the number of miles
 */
 
-function miles(/*add your code here*/){
+function miles(kilometers/*add your code here*/){
+return kilometers * 0.621371
   /*add your code here*/
 }
 
@@ -227,7 +253,8 @@ Using the feet function below do the following:
 3. Return number of feet
 */
 
-function feet(/*add your code here*/){
+function feet(cm/*add your code here*/){
+  return cm / 30.48
   /*add your code here*/
 }
 
