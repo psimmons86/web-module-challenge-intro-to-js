4234862e51fe550818055acf0451ca7eafac8e24
Patrick Simmons <hadroncollides@gmail.com>
2023-01-14 12:31:57 -0800

    completed mvp

--- index.js
+++ index.js
@@ -297,10 +297,25 @@ Using the grade function below do the following:
  below should return 'you got an F'
 */
 
-function grade(/*Your Code here */){
+function grade(number/*Your Code here */){
 /*Your Code here */
+if (number >= 90){
+  return `you got an A`
+}
+else if (number>=80) {
+  return `you got a B`
+}
+else if (number >=70){
+  return `you got a C`
+}
+else if (number >= 60){
+  return `you got a D`
+}
+else {
+  return `you got an F`
 }
 
+}
 
 
 /*💪💪💪💪💪💪💪💪💪💪 Stretch 💪💪💪💪💪💪💪💪💪💪*/
